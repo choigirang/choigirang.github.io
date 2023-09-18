0faca7d5e502e9392a37adbbb5b1d94748eebb47
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-18 22:58:04 +0900

    초기 세팅

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230918.223841';
+const cacheName = 'chirpy-20230918.224702';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
